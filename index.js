301afdf2e0ae4e71271fc9700e1a7a49d7a2e351
Jake <jakespeitel19@gmail.com>
2022-05-20 08:47:55 -0400

    commit tree 08e01c241498c1789ccfc52a9111bb56d43d0852

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+
+function scuberGreetingForFeet(rideLength){
+  if (rideLength < 400 ) {
+    return "This one is on me!";
+  } 
+  if (rideLength >= 400 && rideLength < 2000) {
+    return "That will be twenty bucks.";
+  }
+  if (rideLength >= 2000 && rideLength <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (rideLength > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(currentCity){
+  if(currentCity === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+  if(tip === "generous") {
+    return "Thank you so much.";
+  
+  } else if(tip === "not as generous") {
+      return "Thank you.";
+      
+      } else {
+        return "Bye.";
+        
+}
 }
